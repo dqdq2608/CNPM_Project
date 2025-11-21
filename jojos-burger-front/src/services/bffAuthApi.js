65ae04a7d132ce657b1819a137cce9d0ee503618
dqdq2608 <148328743+dqdq2608@users.noreply.github.com>
2025-11-21 16:18:14 +0700

    modify http

--- jojos-burger-front/src/services/bffAuthApi.js
+++ jojos-burger-front/src/services/bffAuthApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const BFF_BASE =
-  process.env.REACT_APP_API_BASE || "https://localhost:7082";
+  process.env.REACT_APP_API_BASE;
 
 // Axios dùng riêng cho BFF (root)
 const bffHttp = axios.create({
